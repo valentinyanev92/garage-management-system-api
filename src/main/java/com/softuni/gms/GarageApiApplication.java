03a4c528659ee4246dec128f597848aab77f758e
valentinyanev92 <valentinyanev92@gmail.com>
2025-11-01 18:40:25 +0200

    feat: initialize REST microservice project with MongoDB configuration

---
+++ src/main/java/com/softuni/gms/GarageApiApplication.java
@@ -0,0 +1,12 @@
+package com.softuni.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarageApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/softuni/gms/garage_api/GarageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softuni.gms.garage_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
